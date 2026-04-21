8051697180be648ea90c0de56055e37e4b4ce2a0
kalaisanthi29 <ksanthii29@live.com>
2026-04-21 21:16:28 +0200

    Update server.js

--- highsoft-course-portal-main/backend/server.js
+++ highsoft-course-portal-main/backend/server.js
@@ -1,1 +1,12 @@
+import express from "express"
+import courses from "./data/courses.json" assert { type: "json" };
 
+const app = express();
+
+app.get("/api/courses", (req, res => {
+  res.json(courses); 
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000); 
+});
